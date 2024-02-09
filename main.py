48c07cefa6ddcbeca3045782bd5cf35f2d3a080a
IExpLouDI <iexploudi@gmail.com>
2024-02-09 23:14:46 +0400

    SOAP RQ

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-# import xml
-PATH = "chromedriver.exe"
+import requests as rq
+import xml.etree.ElementTree as et
 
-# options = webdriver.ChromeOptions()
-# options.add_argument("start-maximized")
-#
-# driver = webdriver.Chrome()
-#
-# driver.get("https://ya.ru/")
-options = Options()
-options.add_argument("start-maximized")
+degree = '-3'
 
-driver = webdriver.Chrome(options=options)
-driver.implicitly_wait(10)
+url = 'https://www.w3schools.com/xml/tempconvert.asmx'
+SOAPEnvelope = f"""
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <CelsiusToFahrenheit xmlns="https://www.w3schools.com/xml/">
+      <Celsius>{degree}</Celsius>
+    </CelsiusToFahrenheit>
+  </soap:Body>
+</soap:Envelope>
+"""
+options = {"Content-Type": "text/xml; charset=utf-8"}
 
-driver.get('https://mail.ru')
-print("Hello")
+response = rq.post(url, data=SOAPEnvelope, headers=options)
+root = et.fromstring(response.text)
+
+for child in root.iter("{https://www.w3schools.com/xml/}CelsiusToFahrenheitResult"):
+    print(child.text)
